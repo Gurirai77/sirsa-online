2ae3dd5e556626a8a6dd596f98ae4366e1b1d32c
BankTrueSathi <banktruesathi@gmail.com>
2026-03-04 23:02:19 +0530

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://sirsa.online",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sirsa.online/tourism",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sirsa.online/restaurants",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sirsa.online/schools",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sirsa.online/news",
+      lastModified: new Date(),
+    },
+  ];
+}
