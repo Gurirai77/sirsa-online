db4f73f39275360fc914414d44b7d7040277ebfe
BankTrueSathi <banktruesathi@gmail.com>
2026-03-05 14:29:42 +0530

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import type { Metadata } from "next";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Sirsa Online | News, Tourism, Restaurants & Schools in Sirsa",
@@ -15,6 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KVCPKR6W1P"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KVCPKR6W1P');
+          `}
+        </Script>
+      </head>
       <body>
         <Navbar />
         <main className="main-container">{children}</main>
